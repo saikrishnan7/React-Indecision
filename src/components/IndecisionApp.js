7551513e733c84d9b8fd3028d187e8f90c560289
Saikrishnan Srivatsan <Sai_Srivatsan@dell.com>
2019-08-01 20:05:24 -0500

    Testing Github token

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -61,7 +61,7 @@ export default class IndecisionApp extends React.Component
 
     handleAddOption(option) {
         if(!option) {
-            console.log('testing');
+            console.log('testingggg!!!');
             return 'Enter a valid option to add';
         }
         else if(this.state.options.indexOf(option) > -1) {
