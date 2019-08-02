e008f1bc4cfae18ce94da50014491bf19c61ab68
Saikrishnan Srivatsan <Sai_Srivatsan@dell.com>
2019-08-01 19:23:37 -0500

    End of lecture 54

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-import subtract, { square, add } from './utils.js';
-import isSenior, { canDrink, isAdult } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js!!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 23));
-console.log(canDrink(18));
-console.log(isAdult(18));
-console.log(canDrink(21));
-console.log(isAdult(17));
-console.log(isSenior(64));
-console.log(isSenior(67));
+const template = <p>Hi! 123</p>;
+ReactDOM.render(template, document.getElementById('app'));
