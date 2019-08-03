2dbc48625afe205948244b0ad1ab679d9ebf42d7
Saikrishnan Srivatsan <Sai_Srivatsan@dell.com>
2019-08-03 07:01:40 -0500

    Final set of changes for Indecision App

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         contentLabel="Selected Option"
         onRequestClose={props.handleClearModal}
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearModal}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearModal}>Okay</button>
     </Modal>
 );
 
